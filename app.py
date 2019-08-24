cbfff9330ad5dbb6d9dc4a297e0d87c76986b8ac
Meron Desta <meronddesta@gmail.com>
2019-08-23 23:36:29 -0400

    heroku

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template, redirect,current_app
 from flask_pymongo import PyMongo
+import scrape_ads
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds263917.mlab.com:63917/heroku_qkrmp17l"
 mongo=PyMongo(app)
 
 @app.route("/")
 def home():
-
-    return render_template("Index.html")
-
+    return render_template("Index.html",)
 
 #Adding the other pages
-# @app.route("/projectinformation")
-# def trends():
-#     return render_template("Project Information.html")
+@app.route("/projectInformation")
+def projectInformation():
+    return render_template("Project Information.html")
 
 @app.route("/aboutus")
 def aboutus():
